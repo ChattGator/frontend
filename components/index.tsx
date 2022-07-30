55448df2df7d3fe2bbfaf470c33e6bee6e678d7a
Barun Debnath <barundebnath91@gmail.com>
2022-07-30 19:36:36 +0530

    Profile details card done

--- components/index.tsx
+++ components/index.tsx
@@ -4,3 +4,5 @@ export { default as Footer } from "./Footer";
 export { default as ProjectCard } from "./ProjectCard";
 export { default as Input } from "./Input";
 export {default as ProfileCard} from "./ProfileCard"
+export {default as ProfileDetails} from "./ProfileDetails";
+export {default as GroupCard} from "./GroupCard";
